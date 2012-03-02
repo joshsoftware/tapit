c960694715d6fc03e529cbfc152d2e1888949dce
Gautam Rege <gautam@joshsoftware.com>
2012-03-02 13:40:24 +0530

    Setup for Heroku

--- notify.js
+++ notify.js
@@ -7,9 +7,22 @@ app.use(express.bodyParser());
 app.use(express.static(__dirname + '/public'));
 app.set('view engine', 'jade');
 
-app.listen(3000);
+app.listen(80);
 
-const DB = redis.createClient();
+function get_redis_connection() {
+  if (process.env.REDISTOGO_URL) {
+    var rtg   = require("url").parse(process.env.REDISTOGO_URL);
+    var db = redis.createClient(rtg.port, rtg.hostname);
+    db.auth(rtg.auth.split(":")[1]);
+
+    return db;
+  } else {
+    // Development or local machine
+    return redis.createClient();
+  }
+}
+
+const DB = get_redis_connection();
 
 app.get('/', function(req, res) {
   res.render('index');
@@ -39,8 +52,8 @@ app.get('/game/:gameid/:nick', function (req, res) {
 
 io.sockets.on('connection', function(socket) {
   console.log("connected..");
-  const subscribe = redis.createClient();
-  const publish = redis.createClient();
+  const subscribe = get_redis_connection();
+  const publish = get_redis_connection();
 
   socket.on('publish', function(channel, data) {
     publish.publish(channel, data);
